390d6cf2fca8f888b6fbde31892e8129457449b8
Thymangi <154999272+Thymangi@users.noreply.github.com>
2024-05-17 18:56:19 +0200

    premier dépot portfolio

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <cvDo />
+  </div>
+</template>
+
+<script>
+import cvDo from "./components/cvDo.vue";
+export default {
+  name: "App",
+  components: {
+    cvDo,
+  },
+};
+</script>
+
+<style>
+@import url("./style/reset.css");
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap");
+#app {
+  font-family: "Roboto", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/cvDo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="cvDo" class="cv--do">
+    <p>test</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.cv--do {
+  width: 32%;
+  height: 30vh;
+}
+</style>
